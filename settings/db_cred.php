874213fc21277f528bd862715b88a1a572401cf3
Chika Perpetual Amanna <perpetualamanna@gmail.com>
2026-03-16 20:32:55 +0000

    Update database credentials in db_cred.php

--- settings/db_cred.php
+++ settings/db_cred.php
@@ -7,7 +7,7 @@
 
 // 1. Database Server/Host
 if (!defined("DB_SERVER")) {
-    define("DB_SERVER", getenv('MYSQLHOST') ?: "127.0.0.1");
+    define("DB_SERVER", getenv('MYSQLHOST') ?: "centerbeam.proxy.rlwy.net");
 }
 
 // 2. Database Username
@@ -17,16 +17,16 @@ if (!defined("DB_USERNAME")) {
 
 // 3. Database Password
 if (!defined("DB_PASSWORD")) {
-    define("DB_PASSWORD", getenv('MYSQLPASSWORD') ?: "");
+    define("DB_PASSWORD", getenv('MYSQLPASSWORD') ?: "SEzrunnqUlMUQdQgSZKVldLlFHhDTtYu");
 }
 
 // 4. Database Name
 if (!defined("DB_NAME")) {
-    define("DB_NAME", getenv('MYSQLDATABASE') ?: "capstone");
+    define("DB_NAME", getenv('MYSQLDATABASE') ?: "railway");
 }
 
 // 5. Database Port (The missing piece)
 if (!defined("DB_PORT")) {
-    define("DB_PORT", getenv('MYSQLPORT') ?: "3306");
+    define("DB_PORT", getenv('MYSQLPORT') ?: "17600");
 }
 ?>
